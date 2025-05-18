78e527ef8dffe15777b83c25a1ef28d3e5e2de46
Brayan Lozano <brayan.lozano@tinet.cl>
2025-05-17 21:27:21 -0500

    FIX: configuración de puertos

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const URL_BASE = import.meta.env.VITE_API_URL;
+export const URL_BASE = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
 
 export const CITIES: string[] = [
    "Arauca",
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/',
+  server: {
+    port: 5173
+  }
 })
